245d1a8932554457a2e9f50f5adb69cb254ca847
henrysdev <henrysdev@gmail.com>
2018-12-07 00:05:36 -0600

    built rudimentary integration with clj-spotify api

--- recipe/project.clj
+++ recipe/project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-spotify "0.1.8"]]
+  
+  :main ^:skip-aot recipe.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- recipe/src/recipe/core.clj
+++ recipe/src/recipe/core.clj
@@ -1,6 +1,19 @@
-(ns recipe.core)
+(ns recipe.core
+  (:gen-class)
+  (:require [clj-spotify.core :refer :all]
+            [clj-spotify.util :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn grab-track [track-id authtok]
+  (clojure.pprint/pprint (get-a-track {:id track-id} authtok)))
+
+(defn -main [& args]
+  "main method"
+  (def authtok
+    (let [client-id (slurp "debug/client_id")
+          client-secret (slurp "debug/secret")]
+      (get-access-token client-id client-secret)))
+  (with-open [rdr (clojure.java.io/reader "debug/fav_songs")]
+    (doseq [line (line-seq rdr)]
+      (grab-track line authtok))))
+
+;; OAUTH2 => http://leonid.shevtsov.me/post/oauth2-is-easy/
