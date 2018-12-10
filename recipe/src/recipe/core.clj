f7f55b45d81ca426449c8f2ccb686f046db23b03
Henry Warren <henrysdev@gmail.com>
2018-12-10 12:55:55 -0600

    added label retrieval for a given song

--- recipe/src/recipe/core.clj
+++ recipe/src/recipe/core.clj
@@ -3,17 +3,24 @@
   (:require [clj-spotify.core :refer :all]
             [clj-spotify.util :refer :all]))
 
-(defn grab-track [track-id authtok]
-  (clojure.pprint/pprint (get-a-track {:id track-id} authtok)))
+(defn get-track-label [track-id auth-tok]
+  (let [track (get-a-track  {:id track-id} auth-tok)
+        album (get-an-album {:id (:id (:album track))} auth-tok)]
+        (:label album)))
+
+; (defn paginate
+; [page-obj accum]
+; (:next page-obj))
+
 
 (defn -main [& args]
   "main method"
-  (def authtok
+  (def auth-tok
     (let [client-id (slurp "debug/client_id")
           client-secret (slurp "debug/secret")]
       (get-access-token client-id client-secret)))
   (with-open [rdr (clojure.java.io/reader "debug/fav_songs")]
     (doseq [line (line-seq rdr)]
-      (grab-track line authtok))))
+      (println (get-track-label line auth-tok)))))
 
 ;; OAUTH2 => http://leonid.shevtsov.me/post/oauth2-is-easy/
