e131bd7069c12cac47c2064593c11ec5d7aa3194
Henry Warren <henrysdev@gmail.com>
2018-12-10 16:04:06 -0600

    extended to be fully lazy + formatted

--- recipe/src/recipe/core.clj
+++ recipe/src/recipe/core.clj
@@ -8,19 +8,15 @@
         album (get-an-album {:id (:id (:album track))} auth-tok)]
         (:label album)))
 
-; (defn paginate
-; [page-obj accum]
-; (:next page-obj))
-
-
 (defn -main [& args]
-  "main method"
   (def auth-tok
-    (let [client-id (slurp "debug/client_id")
+    (let [client-id     (slurp "debug/client_id")
           client-secret (slurp "debug/secret")]
       (get-access-token client-id client-secret)))
   (with-open [rdr (clojure.java.io/reader "debug/fav_songs")]
-    (doseq [line (line-seq rdr)]
-      (println (get-track-label line auth-tok)))))
+    (->> (line-seq rdr)
+      (map (fn [track-id] (get-track-label track-id auth-tok)))
+      (sort)
+      (println))))
 
 ;; OAUTH2 => http://leonid.shevtsov.me/post/oauth2-is-easy/
