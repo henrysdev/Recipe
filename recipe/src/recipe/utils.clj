6a8c8b11c77145e27789863cdc2adbaa7326fbfe
Henry Warren <henrysdev@gmail.com>
2018-12-19 20:18:43 -0600

    removed debug logic

--- recipe/src/recipe/utils.clj
+++ recipe/src/recipe/utils.clj
@@ -3,7 +3,6 @@
   (:require [clj-http.client :as http]))
 
 (defn- paginate [accum page-obj access-token]
-  (println (:next page-obj))
   (let [accum     (concat accum (:items page-obj))
         next-page (:next page-obj)]
     (if (nil? next-page)
@@ -12,7 +11,7 @@
                                       :as          :json})]
         (paginate accum (:body resp) access-token)))))
 
-(defn fetch-all [api-fn params access-token]
+(defn unpage [api-fn params access-token]
   (let [page-obj   (api-fn params access-token)
         accum-objs (paginate [] page-obj access-token)]
         accum-objs))
